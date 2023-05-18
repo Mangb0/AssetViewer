a937e8b46ace9149d11f1f9906c414a2e924cc4a
Mangbo <whdudghks45629@gmail.com>
2023-05-18 22:50:25 +0900

    rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   outputDir: "./docs",
+  publicPath: "/<repository name>/",
   transpileDependencies: true,
   chainWebpack: (config) => {
     config.module
